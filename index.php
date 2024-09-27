1e99420e30270a1c91c8ac875e65f3c613fe80b1
Jrkatigbak <jrey.katigbak1996@gmail.com>
2024-09-28 01:08:41 +0800

    Adding a Modal and Table

--- index.php
+++ index.php
@@ -21,9 +21,55 @@
         <button class="btn-spin" >Spin the wheel</button>
        
     </div>
+    <a href="#" id="openModalButton" class="text-center options">Manage Teams</a>
+    <div id="modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Basketball Teams</h2>
+        <table>
+            <tr>
+                <th>Team</th>
+                <th>Color <br>  (Hex Value Only)</th>
+                <th>Option</th>
+            </tr>
+            <?php
+            // Loop through the prizes and generate table rows
+            foreach ($prizes as $prize) {
+                echo '<tr  style="background-color:' . htmlspecialchars($prize['color']) . ';">';
+                    echo '<td>' . htmlspecialchars($prize['text']) . '</td>';
+                    echo '<td>' . htmlspecialchars($prize['color']) . '</td>';
+                    echo '<td>
+                          </td>';
+                echo '</tr>';
+            }
+            ?>
+            
+        </table>
+    </div>
+    </div>
     <script>
         var options = <?= $jsonPrizes; ?>;
-        // alert(options);
+
+        // MODAL
+        var modal = document.getElementById("modal");
+        var btn = document.getElementById("openModalButton");
+        var span = document.getElementsByClassName("close")[0];
+
+        btn.onclick = function() {
+        modal.style.display = "block";
+        }
+
+        span.onclick   
+        = function() {
+        modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";   
+
+        }
+        }
     </script>
     <script src="spin-wheel.js"></script>
 </body>
